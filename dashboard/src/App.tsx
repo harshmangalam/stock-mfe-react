9c92209c7d779bc54cadad4014707bf981b65484
harshmangalam <harshdev8218@gmail.com>
2022-08-10 13:59:01 +0530

    removed unused css

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import "./index.css";
 
 const theme = createTheme({
   palette: {
