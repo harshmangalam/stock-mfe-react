0e4cd57a9319083c4b60ca02546b881730050877
harshmangalam <harshdev8218@gmail.com>
2022-08-10 14:02:51 +0530

    added navbar with logo and logout

---
+++ dashboard/src/components/Navbar.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import LogoutIcon from "@mui/icons-material/Logout";
+
+export default function Navbar() {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Dashboard
+        </Typography>
+        <Button color="error" endIcon={<LogoutIcon />}>
+          Logout
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
+}
