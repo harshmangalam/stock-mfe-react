d412979038ccfaa52bdae7cea17ca5309f7e9ad6
harshmangalam <harshdev8218@gmail.com>
2022-08-10 14:04:22 +0530

    added home dashboard

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
+import { Box } from "@mui/material";
+import Navbar from "./components/Navbar";
 
 const theme = createTheme({
   palette: {
@@ -12,6 +14,10 @@ const theme = createTheme({
 const App = () => (
   <ThemeProvider theme={theme}>
     <CssBaseline />
+    <Box>
+      <Navbar />
+      <Box component={"main"}>Dashboard main</Box>
+    </Box>
   </ThemeProvider>
 );
 ReactDOM.render(<App />, document.getElementById("app"));
